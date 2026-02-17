09e13565e8e915d9edc1578981da8baa730ce821
Johann Paimboeuf <johann.paimboeuf@gmail.com>
2026-02-17 11:40:19 +0100

    incremental text

--- Assets/scripts/Wallet.cs
+++ Assets/scripts/Wallet.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
+using TMPro;
 
 public class Wallet : MonoBehaviour
 {
-    private int wallet = 0;
+    private float wallet = 0.0f;
     private float timing = 1.0f;
     private float timer = 0.0f;
+    [SerializeField] 
+    private TMP_Text _walletText;
 
     public void AddDollars()
     {
         wallet += 1;
+        DisplayWallet();
         Debug.Log("----------" + wallet + "----------------");
     }
 
@@ -20,10 +24,17 @@ public class Wallet : MonoBehaviour
         if (timer >= timing)
         {
             wallet += 1;
+            DisplayWallet();
             Debug.Log(wallet);
             timer = 0;
         }
 
+    }
+
+    public void DisplayWallet()
+    {
+        _walletText.SetText("${0}", wallet);
+        
     }
 
     public void Update()
