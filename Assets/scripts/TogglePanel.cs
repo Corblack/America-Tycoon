f8b05a7ce940bdfcca9d1e283643bdfff561f60f
Johann Paimboeuf <johann.paimboeuf@gmail.com>
2026-02-19 03:34:30 +0100

    adding functional settings button

--- Assets/scripts/TogglePanel.cs
+++ Assets/scripts/TogglePanel.cs
@@ -5,10 +5,19 @@ public class TogglePanel : MonoBehaviour
     [SerializeField]
     private GameObject upgradePanel;
 
-    public void SwitchPanel()
+    [SerializeField]
+    private GameObject SettingsPanel;
+
+    public void UpgradeSwitchPanel()
     {
         bool isActive = upgradePanel.activeSelf;
         upgradePanel.SetActive(!isActive);
+    }
+
+        public void SettingsSwitchPanel()
+    {
+        bool isActive = SettingsPanel.activeSelf;
+        SettingsPanel.SetActive(!isActive);
     }
 
 }
